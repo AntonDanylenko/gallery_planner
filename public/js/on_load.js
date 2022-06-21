1f1107c3f5cea089456990701acf5d3e31a053ac
Anton D <adanylenko29@gmail.com>
2022-06-21 21:18:23 +0200

    added page loading screen

---
+++ public/js/on_load.js
@@ -0,0 +1,8 @@
+// LOAD PAGE
+
+var show_page = function(e) {
+  document.getElementById("loadingText").style.display = "none";
+  document.getElementById("main").style.display = "block";
+}
+
+window.onload = show_page;
